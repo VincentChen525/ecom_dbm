6f23f23395c2189e90cad37db94d5c131fa2ee10
VincentChan <276561900@qq.com>
2017-11-27 21:40:42 -0500

    first commit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/VincentChan/Documents/Pitt/Database/dj_ecom_web/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
